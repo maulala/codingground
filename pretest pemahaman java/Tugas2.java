e15e7672c459de93438d30f7864e910937fb4bac
maulala <maula.hashina@gmail.com>
2017-02-27 12:32:38 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas2.java
@@ -0,0 +1,11 @@
+public class Tugas2{
+    
+ public static void main (String[]args){
+ String[]nama = {"lala" , "lili" , "lulu"};
+ 
+ System.out.println("nama 1 = "+ nama[0]);
+ System.out.println("nama 2 = "+ nama[1]);
+ System.out.println("nama 3 = "+ nama[2]);
+ 
+ }
+}
