432cf494f41aa0ca84bd381ac54d2b682e1df6a4
maulala <maula.hashina@gmail.com>
2017-02-27 12:36:06 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas4.java
@@ -0,0 +1,12 @@
+public class Tugas4{
+
+     public static void main(String []args)
+     { 
+         int hitung = Penjumlahan(1,2);
+         System.out.println(hitung);
+     }
+     
+     public static int Penjumlahan(int n1, int n2){    
+        return n1+n2;
+     }
+}
