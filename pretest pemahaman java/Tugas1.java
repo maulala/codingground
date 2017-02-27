6dea3918899fe2ef6b2e65513d00f78f70197a2e
maulala <maula.hashina@gmail.com>
2017-02-27 10:54:59 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas1.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+         String nama = "Maula H.D";
+         String kota = "Jepara";
+        System.out.println("Nama : "+nama+"Kota : "+kota);
+     }
+}
