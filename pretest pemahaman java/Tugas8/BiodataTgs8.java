0a8beb69ad511ae0db035da04fadd8a86e3a662f
maulala <maula.hashina@gmail.com>
2017-03-05 12:59:37 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas8/BiodataTgs8.java
@@ -0,0 +1,9 @@
+public class BiodataTgs8{
+	String nama = "Maula H.D";
+	String alamat = "Jepara";
+	int umur = 20;
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
+	}
+}
