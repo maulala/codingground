689f3f08d4a8a98a96dd9017f7bf98ff6a2b5c60
maulala <maula.hashina@gmail.com>
2017-03-05 13:53:26 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas11/Mahasiswa.java
@@ -0,0 +1,8 @@
+public class Mahasiswa extends BiodataTgs11{
+	String nim = "141240000313";
+	String jurusan = "Teknik Informatika";
+
+	void CetakBiodata(){
+		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur + "\nNIM : " + nim + "\nJurusan : " + jurusan);
+	}
+}
